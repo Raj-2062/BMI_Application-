6726c486f481767805f8d31b711d2e564cacf17a
Raj-2062 <dewanabdulkhalikraj@gmail.com>
2025-06-01 20:40:25 +0600

    forth

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   var h1 = TextEditingController();
   var w1 = TextEditingController();
+  var result = "";
+  var info = "";
   @override
   Widget build(BuildContext context) {
 
@@ -51,12 +53,12 @@ class _MyHomePageState extends State<MyHomePage> {
               Padding(
 
                 padding: const EdgeInsets.all(8.0),
-                child: Text("Height(m)",style: TextStyle(fontSize: 12,color: Colors.black,),),
+                child: Text("Weight(kg)",style: TextStyle(fontSize: 12,color: Colors.black,),),
               ),
               TextField(
-                controller: h1 ,
+                controller: w1 ,
                 decoration: InputDecoration(
-                  hintText: "Width",
+                  hintText: "Weight",
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12),
                     borderSide: BorderSide(color: Colors.black),
@@ -75,12 +77,12 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text("Width(kg)",style: TextStyle(fontSize: 12,color: Colors.black),),
+                child: Text("Height(ft)",style: TextStyle(fontSize: 12,color: Colors.black),),
               ),
               TextField(
-                controller: w1 ,
+                controller: h1 ,
                 decoration: InputDecoration(
-                  hintText: "Width",
+                  hintText: "Height",
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12),
                       borderSide: BorderSide(color: Colors.black),
@@ -93,11 +95,59 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
               Center(
-                child: ElevatedButton(onPressed: (){
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(onPressed: (){
 
-                }, 
-                    child:Text("Click For BMI") ),
+                    try {
+                      var a = double.parse(w1.text.trim());
+                      var b = double.parse(h1.text.trim());
+                      b = b*0.3048;
+                      var bmi = a / (b * b);
+                      result = bmi.toStringAsFixed(2);
+                      if(bmi < 18.5) {
+                        info = "You are UNDERWEIGHT";
+                      }
+                      else if (bmi > 18.5 && bmi < 25 ) {
+                        info = "You are in Normal weight";
+                      }
+                      else if (bmi >= 25 && bmi < 30) {
+                        info = "You are OVERWEIGHT";
+                      }
+                      else if (bmi >= 30 && bmi < 35) {
+                        info = "You are in Obesity Class I ";
+                      }
+                      else if (bmi >= 35 && bmi < 40) {
+                        info = "You are Obesity Class II";
+                      }
+                      else if (bmi >= 40) {
+                        info = "You are Obesity Class III";
+                      }
+
+                    } catch (e) {
+                      result = "Invalid input";
+                    }
+                    setState(() {});
+                  },
+                      child:Text("Click For BMI") ),
+                ),
+              ),
+              Padding(
+
+                padding: const EdgeInsets.all(10.0),
+                child: Text("The  BMI is $result",style: TextStyle(fontSize: 17,color:Colors.black),),
+              ),
+
+              SizedBox(
+                height: 20,
+
+
+              ),
+              Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Text(info,style: TextStyle(fontSize: 17, color: Colors.black),),
               )
+
             ],
           ),
         ),
